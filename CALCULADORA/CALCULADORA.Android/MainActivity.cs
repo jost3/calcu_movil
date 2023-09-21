f1abf5c58d6b28e52804bf96f74d3a8263712b50
jost3 <1427593@senati.pe>
2023-09-21 17:15:31 -0500

    agregando img del aplicativo

--- CALCULADORA/CALCULADORA.Android/MainActivity.cs
+++ CALCULADORA/CALCULADORA.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace CALCULADORA.Droid
 {
-    [Activity(Label = "CALCULADORA", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "CALCULADORA", Icon = "@mipmap/calcu", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
